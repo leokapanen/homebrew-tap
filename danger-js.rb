6788456ee52c59727ccabd7465afbcad84e9a10e
Leonid Kapanen <leokapanen@gmail.com>
2023-01-30 12:05:34 +0100

    Switching to "Releasing danger-swift version 3.14.2"

--- danger-js.rb
+++ danger-js.rb
@@ -1,21 +1,9 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
+  url "https://github.com/danger/danger-js/releases/download/11.1.4/danger-macos.zip"
+  sha256 "ad42a8603dea80f33a236acc385206caefd8942aed08ae26428e0097bf072486"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/danger/danger-js/releases/download/11.2.1/danger-macos-x64.zip"
-    sha256 "31f33cdf63cdc47d8f07c517ac2b15a704a18aded19cbdfd6a2114235b6c6b64"
-
-    def install
-      bin.install "danger-x64" => "danger"
-    end
-  end
-
-  if Hardware::CPU.arm?
-    url "https://github.com/danger/danger-js/releases/download/11.2.1/danger-macos-arm64.zip"
-    sha256 "ac809811d4cb9604a48eac9d8b24c01f54a5d32a3e12aaccd438b88eb7128b57"
-
-    def install
-      bin.install "danger-arm64" => "danger"
-    end
+  def install
+    bin.install "danger"
   end
 end
